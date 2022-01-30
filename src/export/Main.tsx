eb29e63958d7448366f188bfbaf442af0efc4b52
Kennedy Paixão <kennedympaixao@gmail.com>
2022-01-30 03:29:03 -0300

    Criando tela de login

--- src/export/Main.tsx
+++ src/export/Main.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 import { Theme } from 'themes';
 import { PageRoutes } from 'routes'
 
+import './Main.scss';
+
 const Main: React.FC = () => {
   return (
     <Theme>
-      <div>
-        aaaaa
-      </div>
       <PageRoutes></PageRoutes>
     </Theme>
   );
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,10 +1,49 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faGoogle} from '@fortawesome/free-brands-svg-icons';
+import { faLock, faUser } from '@fortawesome/free-solid-svg-icons';
+
+import './Login.scss';
 
 const Login: React.FC = () => {
   return (
-    <div>
-      Login
-    </div>
+    <section className="Login">
+      <div className="card">
+        <h2 className="title">Bora Mochilar</h2>
+        <h2 className="brand-title">Login</h2>
+
+        <form>
+          <div className="form-box">
+            <span>E-mail</span>
+            <div className="content-form-box">
+              <FontAwesomeIcon icon={faUser} color="#a64bf4" />
+              <input type='text' placeholder="Informe seu e-mail" />
+              <div className="bar" />
+            </div>
+          </div>
+          <div className="form-box">
+            <span>Senha</span>
+            <div className="content-form-box">
+              <FontAwesomeIcon icon={faLock} color="#a64bf4" />
+              <input type='password' placeholder="Informe sua senha" />
+              <div className="bar" />
+            </div>
+          </div>
+          <button className="btn-forget-pwd">Esqueci a senha</button>
+
+          <div className="container-login100-form-btn">
+            <div className="wrap-login100-form-btn">
+              <div className="login100-form-bgbtn"></div>
+              <button className="login100-form-btn">
+               Login
+              </button>
+            </div>
+          </div>
+
+        </form>
+
+      </div>
+    </section>
   )
 }
 
